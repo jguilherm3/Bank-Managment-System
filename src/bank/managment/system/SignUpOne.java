cbe348481f4ef14c93faa58e0f7225fcc922be8e
Joao Guilherme <jguilhermemdc@gmail.com>
2026-07-14 15:16:18 -0300

    Finalizado a parte de Sing Up

---
+++ src/bank/managment/system/SignUpOne.java
@@ -0,0 +1,185 @@
+package bank.managment.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import com.toedter.calendar.JDateChooser;
+
+public class SignUpOne extends JFrame {
+
+    SignUpOne() {
+
+        setLayout(null);
+
+
+        Random ran = new Random();
+        // número longo, mas deixando apenas com 4 casas
+        long randomNumber = Math.abs((ran.nextLong() % 9000L)+ 1000L);
+
+        JLabel formNumber = new JLabel("APPLICATION FORM No."+randomNumber);
+        formNumber.setFont(new Font("Raleway", Font.BOLD, 30));
+        formNumber.setBounds(170, 20, 600, 40);
+        add(formNumber);
+
+        JLabel personDetails = new JLabel("Page 1: Personal Details");
+        personDetails.setFont(new Font("Raleway", Font.BOLD, 22));
+        personDetails.setBounds(290, 80, 400, 30);
+        add(personDetails);
+
+        // Campos de nome
+        JLabel name = new JLabel("Name:");
+        name.setFont(new Font("Raleway", Font.BOLD, 20));
+        name.setBounds(100, 140, 100, 30);
+        add(name);
+
+        JTextField nameTextField = new JTextField();
+        nameTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+        nameTextField.setBounds(300, 140, 400, 30);
+        add(nameTextField);
+
+
+        // Campos de Nome do Pai
+        JLabel FatherName = new JLabel("Father's Name:");
+        FatherName.setFont(new Font("Raleway", Font.BOLD, 20));
+        FatherName.setBounds(100, 190, 200, 30);
+        add(FatherName);
+
+        JTextField fnameTextField = new JTextField();
+        fnameTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+        fnameTextField.setBounds(300, 190, 400, 30);
+        add(fnameTextField);
+
+        // Campos de Data de Nascimento
+        JLabel dob = new JLabel("Date of Birth:");
+        dob.setFont(new Font("Raleway", Font.BOLD, 20));
+        dob.setBounds(100, 240, 200, 30);
+        add(dob);
+
+        // IMPORTAÇÃO DO ARQUIVO jcalendar PARA USO DE CALENDÁRIOS
+        JDateChooser dateChooser = new JDateChooser();
+        dateChooser.setBounds(300,240,400,30);
+        dateChooser.setForeground(new Color(105,105,105));
+        add(dateChooser);
+
+        // BOTÕES DO TIPO RADIO NO GêNERO
+        JLabel gender = new JLabel("Gender:");
+        gender.setFont(new Font("Raleway", Font.BOLD, 20));
+        gender.setBounds(100, 290, 200, 30);
+        add(gender);
+
+        JRadioButton male = new JRadioButton("Male");
+        male.setBounds(300, 290, 60, 30);
+        male.setBackground(Color.white);
+        add(male);
+
+        JRadioButton female = new JRadioButton("Female");
+        female.setBounds(400, 290, 80, 30);
+        female.setBackground(Color.white);
+        add(female);
+
+        // PARA SELECIONAR APENAS UM BOTÃO GÊNERO
+        ButtonGroup gendergroup = new ButtonGroup();
+        gendergroup.add(male);
+        gendergroup.add(female);
+
+        //Campos de Email
+        JLabel email = new JLabel("Email Address:");
+        email.setFont(new Font("Raleway", Font.BOLD, 20));
+        email.setBounds(100, 340, 200, 30);
+        add(email);
+
+        JTextField emailTextField = new JTextField();
+        emailTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+        emailTextField.setBounds(300, 340, 400, 30);
+        add(emailTextField);
+
+        // Campos de Estado civil
+        JLabel marital = new JLabel("Marital Status:");
+        marital.setFont(new Font("Raleway", Font.BOLD, 20));
+        marital.setBounds(100, 390, 200, 30);
+        add(marital);
+
+        JRadioButton married = new JRadioButton("Married");
+        married.setBounds(300, 390, 100, 30);
+        married.setBackground(Color.white);
+        add(married);
+
+        JRadioButton unmarried = new JRadioButton("Unmarried");
+        unmarried.setBounds(400, 390, 100, 30);
+        unmarried.setBackground(Color.white);
+        add(unmarried);
+
+        JRadioButton other = new JRadioButton("Other");
+        other.setBounds(500, 390, 100, 30);
+        other.setBackground(Color.white);
+        add(other);
+
+        // PARA SELECIONAR APENAS UM BOTÃO DE ESTADO CIVIL
+        ButtonGroup maritalgroup = new ButtonGroup();
+        maritalgroup.add(married);
+        maritalgroup.add(unmarried);
+        maritalgroup.add(other);
+
+        // Endereço do usuário
+        JLabel address = new JLabel("Address:");
+        address.setFont(new Font("Raleway", Font.BOLD, 20));
+        address.setBounds(100, 440, 200, 30);
+        add(address);
+
+        JTextField addressTextField = new JTextField();
+        addressTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+        addressTextField.setBounds(300, 440, 400, 30);
+        add(addressTextField);
+
+        //Campo de Cidade
+        JLabel city = new JLabel("City:");
+        city.setFont(new Font("Raleway", Font.BOLD, 20));
+        city.setBounds(100, 490, 200, 30);
+        add(city);
+
+        JTextField cityTextField = new JTextField();
+        cityTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+        cityTextField.setBounds(300, 490, 400, 30);
+        add(cityTextField);
+
+        //Estado
+        JLabel state = new JLabel("State:");
+        state.setFont(new Font("Raleway", Font.BOLD, 20));
+        state.setBounds(100, 540, 200, 30);
+        add(state);
+
+        JTextField stateTextField = new JTextField();
+        stateTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+        stateTextField.setBounds(300, 540, 400, 30);
+        add(stateTextField);
+
+        JLabel pinCode = new JLabel("Pin Code:");
+        pinCode.setFont(new Font("Raleway", Font.BOLD, 20));
+        pinCode.setBounds(100, 590, 200, 30);
+        add(pinCode);
+
+        JTextField pinTextField = new JTextField();
+        pinTextField.setFont(new Font("Raleway", Font.BOLD, 14));
+        pinTextField.setBounds(300, 590, 400, 30);
+        add(pinTextField);
+
+        JButton next = new JButton("Next");
+        next.setBackground(Color.BLACK);
+        next.setForeground(Color.WHITE);
+        next.setFont(new Font("Raleway", Font.BOLD, 15));
+        next.setBounds(620, 660, 80, 30);
+        add(next);
+
+
+        getContentPane().setBackground(Color.WHITE);
+
+        setSize(850,800);
+        setLocation(350,10);
+        setVisible(true);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+        new SignUpOne();
+    }
+}
